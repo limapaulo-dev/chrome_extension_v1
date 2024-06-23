31fd3c00f8536a1dada950a9dbe9edf7cae21c68
paulo limae <limapaulo.dev@gmail.com>
2024-06-23 16:13:03 -0300

    remove comment

--- app/worker/background.js
+++ app/worker/background.js
@@ -1,19 +1,3 @@
-/* let data = {
-
-    'prefs': {
-        'recently_impersonated': [],
-        'pinned-group': [],
-        'list-group': []
-    }
-
-    'account': {
-        'acc_id': #,
-        'acc_name': 'string',
-        'acc_class_order': 'account-#'
-    }
-
-}; */
-
 chrome.runtime.onMessage.addListener((data) => {
 	switch (data.event) {
 		case 'save':
