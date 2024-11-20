aa7ce60fde910ba26f4d56b87a6801a676ef85d0
paulo limae <limapaulo.dev@gmail.com>
2024-11-20 11:30:44 -0300

    storage.sync

--- app/worker/background.js
+++ app/worker/background.js
@@ -2,10 +2,11 @@ chrome.runtime.onMessage.addListener((data) => {
 	switch (data.event) {
 		case 'save':
 			chrome.storage.local.set(data.prefs);
+			chrome.storage.sync.set(data.prefs);
+			
 			break;
 
 		case 'load':
-
 			break;
 
 		default:
